4bec698af4f8a8fc4aadaf858f70cfb7d9c81ba8
edgargv <l16140654@queretaro.tecnm.mx>
2021-07-15 13:43:10 -0500

    Primer fase de Test en APIs - Primer Reporte Gmail y TestOps

---
+++ Test Suites/FVAutorizacionesReferencia/TS_FVAutorizacionesReferencia.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_FVAutorizacionesReferencia</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>edgargv537@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e640f469-7602-4282-b231-c0527becbfec</testSuiteGuid>
+   <testCaseLink>
+      <guid>322173ca-c82f-4391-933c-fdc97d9bc0dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FVAutorizacionesReferencia/getConsultaReferenciaAut</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
